41c2e788fcd693233d5daf885076b5f2c8166ea3
abhilash148 <saiabhilash148@gmail.com>
2023-04-13 02:21:19 -0500

    04-13-23: API call added along with navigation

--- Users/ContentView.swift
+++ Users/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var users = Users()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(users.users, id: \.id) { user in
+                    NavigationLink {
+                        Text(user.name)
+                    } label: {
+                        Text(user.name)
+                    }
+                }
+            }
+            .navigationTitle("Users")
+        }
+        .onAppear {
+            Task {
+                await loadData()
+            }
         }
     }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            let decoder = try JSONDecoder().decode([User].self, from: data)
+            self.users.users = decoder
+        } catch {
+            print("Abhilash error: \(error.localizedDescription)")
+        }
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Users/Models/Users.swift
@@ -0,0 +1,15 @@
+//
+//  Users.swift
+//  Users
+//
+//  Created by Sai Abhilash Gudavalli on 13/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+class Users: ObservableObject {
+    
+    @Published var users: [User] = []
+    
+}
