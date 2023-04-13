4ef7428afeafcf9bc4cc2e22c6656a619adda8d8
abhilash148 <saiabhilash148@gmail.com>
2023-04-12 22:39:56 -0500

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Sai Abhilash Gudavalli on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
